d958eabb62a53f70972f35030cd69acffbf73372
100jnghn <gnswhd0415@gmail.com>
2024-12-11 19:38:35 +0900

    REFACTOR : normal ps 수정
    [normalPS.hlsl]
     - 더이상 Phong 연산을 수행하지 않음
     - 변수 이름, 주석 처리 제거

--- DX11_Chap8_NormalMap/normalPS.hlsl
+++ DX11_Chap8_NormalMap/normalPS.hlsl
@@ -50,18 +50,18 @@ float4 NormalMapPixelShader(PixelInputType input) : SV_TARGET
     float4 bumpMap;
     float3 bumpNormal;
     
+    // ----- 조명1 ----- //
     float3 lightDir1;
     float lightIntensity1;
     
-    // ----- Phong 조명값 ----- //
+    // ----- 조명2 ----- //
     float3 lightDir2;
     float lightIntensity2;
-    // ----------------------- //
     
+    // 최종 결과를 저장할 변수
     float4 color;
-    
-    float4 ambientColor;
 
+    
 	
 	// Sample the pixel color from the color texture at this location.
     textureColor = shaderTexture1.Sample(SampleType, input.tex);
@@ -90,36 +90,14 @@ float4 NormalMapPixelShader(PixelInputType input) : SV_TARGET
     lightIntensity2 = saturate(dot(bumpNormal, lightDir2));
     
     
-    /*
-    // ----- Phong 조명 설정 ----- //
-    lightDir2 = -lightDirection2;
-    lightDir2 = normalize(lightDir2);   // 방향 벡터 정규화
-    
-    lightIntensity2 = saturate(dot(bumpNormal, lightDir2)); // Diffuse  // Normal & Dir 벡터 내적 후 0~1값으로 변환
-    //lightIntensityPhong = saturate(dot(input.normal, lightDirPhong)); 
-    
-    float3 viewingVec = normalize(-input.position.xyz); // View 벡터 == input의 반대 방향
-    
-    //float3 reflectionVec = reflect(lightDirPhong, input.normal);        // Reflect 벡터   // input.normal 기준
-    float3 reflectionVec = reflect(lightDir2, bumpNormal);          // Reflect 벡터   // bump normal 기준
-    
-    float sLightIntensity2 = saturate(pow(dot(reflectionVec, viewingVec), 16)); // Specular // Reflect & View 벡터 내적 후 0~1값 변환 후 지수 연산
-    // -------------------------- //
-    
-    
-    
-    color = saturate(diffuseColor1 * lightIntensity1 + (diffuseColor2 * lightIntensity2 + diffuseColor2 * sLightIntensity2));
-    //color = saturate(diffuseColor * lightIntensity + diffuseColorPhong * sLightIntensityPhong);
-    */
-
-    
-    
+ 
 	// Combine the final light color with the texture color.
     float4 color1 = saturate(diffuseColor1 * lightIntensity1);
     float4 color2 = saturate(diffuseColor2 * lightIntensity2);
     color = saturate(color1 + color2);
-    color = color * textureColor * 1.3;     // 1.3은 임시 값!
+    color = color * textureColor * 1.3;     // 1.3은 임의의 값
 
-    //return textureColor;
+
+    
     return color;
 }
